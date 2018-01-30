7392ae13e08ecc4ee2f9aa20d22c1b1175176398
박세령 <parksr2004@naver.com>
2018-01-30 20:28:05 +0900

    commit
    commit

--- Android/app/src/main/java/com/example/jaehyung/seniorproject/MainActivity.java
+++ Android/app/src/main/java/com/example/jaehyung/seniorproject/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.jaehyung.seniorproject;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button conbtn;
 
     int BackButtonCounter =0;
     @Override
@@ -16,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         BackButtonCounter =0;
         setContentView(R.layout.activity_main);
+        conbtn=(Button)findViewById(R.id.conbtn);
+        conbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
