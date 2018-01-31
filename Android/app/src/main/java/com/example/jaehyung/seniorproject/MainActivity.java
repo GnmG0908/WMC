4b9557e95a8307370b59a65b22974280cca578f8
jae637 <jae637@naver.com>
2018-01-31 12:54:44 +0900

    ??
    testing

--- Android/app/src/main/java/com/example/jaehyung/seniorproject/MainActivity.java
+++ Android/app/src/main/java/com/example/jaehyung/seniorproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jaehyung.seniorproject;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,11 +17,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
 
+public class MainActivity extends Activity {
+
+    static  final  int REQUEST_ENABLE_BT = 10;
+    int mPariedDeviceCount= 0;
+    Set<BluetoothDevice> mBluetoothAdapterSet;
+    BluetoothAdapter mBluetoothAdapter;
     Button conbtn;
+    BluetoothDevice mRemoteDevie;
+    BluetoothSocket mSocket= null;
+    OutputStream mOutputStream= null;
+    InputStream mInputStream = null;
+    String mStrDlimiter = "\n";
+    char mCharDlimiter = '\n';
 
-    int BackButtonCounter =0;
+    Thread mWorkerThread = null;
+    byte[] readBuffer;
+    int readBufferPosition;
+
+    int BackButtonCounter =0;   // 뒤로가기 키 제어
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    BluetoothDevice getDeviceFromBondedList(String name){
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
