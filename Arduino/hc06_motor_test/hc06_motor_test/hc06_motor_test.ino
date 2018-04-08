d79e22fae8ba1556e5ab2a19beeab0ae48b5a60a
GnmG0908 <34735170+GnmG0908@users.noreply.github.com>
2018-04-08 20:22:12 +0900

    stringMotor
    Motor move with string

--- Arduino/hc06_motor_test/hc06_motor_test/hc06_motor_test.ino
+++ Arduino/hc06_motor_test/hc06_motor_test/hc06_motor_test.ino
@@ -1,53 +1,75 @@
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 
 #define dir1 8
-#define dir2 10
+//#define dir2 10
 #define pwm1 9
-#define pwm2 11
+//#define pwm2 11
 
-#define RXD 8 //아두이노에서는 7
-#define TXD 7 //아두이노에서는 8
+//#define RXD 8 //아두이노에서는 7
+//#define TXD 7 //아두이노에서는 8
 //http://www.micro4you.com/files/ElecFreaks/Bluetooth%20HC-06.pdf
 
-SoftwareSerial bluetooth(RXD, TXD);
+//SoftwareSerial bluetooth(RXD, TXD);
 int pwm_value;
-byte buffer[20]={0};
-int bufferPosition;
-char temp[20];
+
+String readSerial(){
+  String str = "";
+  char data;
+  while (Serial.available()){
+    data = Serial.read();
+    str.concat(data);
+    delay(10);
+  }
+  if (str != ""){
+    Serial.println(str);
+  }
+  return str;
+}
 
 void setup() {
   Serial.begin(9600);
-  bluetooth.begin(9600);
+  //bluetooth.begin(9600);
   pinMode(pwm1, OUTPUT);
-  pinMode(pwm2, OUTPUT);
-  bufferPosition = 0;
+  //pinMode(pwm2, OUTPUT);
 }
 
+void loop(){
+  String str = readSerial();
+  if (str == "on"){
+    Serial.println("전진");
+    digitalWrite(dir1, HIGH);
+    analogWrite(pwm1, 255);
+    delay(5000);
+    analogWrite(pwm1, 0);
+  }
+}
+/*
 void loop() {  
   // phone -> arduino
-  if(bluetooth.available()){
-    byte data = bluetooth.read();
-    //Serial.write(data);
-    buffer[bufferPosition++] = data;
-    if (data == '\n'){
-      buffer[bufferPosition] = '\0';
-      //Serial.write(buffer, bufferPosition);
-      //Serial.print((char*)buffer);
-      bufferPosition = 0;
-      strcpy(temp, (char*)buffer);
-      Serial.println(temp);
-      if (strcmp("on", temp) == true){
-        Serial.println("주행");
-        digitalWrite(dir1, LOW);
-        digitalWrite(dir2, HIGH);
-        analogWrite(pwm1, 255);
-        analogWrite(pwm2, 255);
-      }
+  //if(bluetooth.available()){
+  if (Serial.available()){
+    while ((data = Serial.read()) != '\n'){
+      //char data = Serial.read();
+      //char data = bluetooth.read();
+      buffer[bufferPosition] = data;
+      bufferPosition++;
+      Serial.print(data);
     }
-    //Serial.write(bluetooth.read());
+    //buffer[bufferPosition] = '\0';
   }
+  
+  if (!strcmp(buffer, "on")){
+    Serial.println("주행");
+    digitalWrite(dir1, LOW);
+    //digitalWrite(dir2, HIGH);
+    analogWrite(pwm1, 255);
+    //analogWrite(pwm2, 255);
+  }
+  bufferPosition = 0;
+  memset(buffer, 0, sizeof(buffer));
+  
   // arduino -> phone
   if(Serial.available()){
     bluetooth.write(Serial.read());
-  }
-}
+  }  
+}*/
