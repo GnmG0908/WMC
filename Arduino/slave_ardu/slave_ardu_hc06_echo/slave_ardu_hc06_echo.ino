d03c8c78f6de1fae5c19326bc8ee110a2346fe24
GnmG0908 <34735170+GnmG0908@users.noreply.github.com>
2018-04-29 19:52:04 +0900

    초음파/주석
    초음파 소스 일부 추가
    주석 추가
    시작

--- Arduino/slave_ardu/slave_ardu_hc06_echo/slave_ardu_hc06_echo.ino
+++ Arduino/slave_ardu/slave_ardu_hc06_echo/slave_ardu_hc06_echo.ino
@@ -1,21 +1,26 @@
 #include <Wire.h>
-
+// 슬레이브
 #define SLAVE 1
+// 측 모터
+#define dir1 10
+#define pwm1 11
+// 측 모터
+#define dir2 12
+#define pwm2 13
+// 좌상 초음파
+#define echoPinLU 2
+#define trigPinLU 3
+// 우상 초음파
+#define echoPinRU 4
+#define trigPinRU 5
 
-#define dir1 8
-#define pwm1 9
-#define dir2 10
-#define pwm2 11
-#define echoPinR 4
-#define trigPinR 5
-#define echoPinL 6
-#define trigPinL 7
-
+// 변수들
 String str = "";
-int pwm_value;
-long durationR, durationL;
-float distanceR, distanceL;
+//int pwm_value;
+long durationLU, durationRU;
+float distanceLU, distanceRU;
 
+// 마스터로부터 텍스트 수신
 void receiveFromMaster(int bytes) {
   char c;
   for (int i = 0 ; i < bytes ; i++) {
@@ -26,7 +31,46 @@ void receiveFromMaster(int bytes) {
   active();
   str = "";
 }
+/*
+// 좌상 초음파 측정
+boolean echoLU(){
+  digitalWrite(trigPinLU, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinLU, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinLU, LOW);
+  durationLU = pulseIn(echoPinLU, HIGH);
+  distanceLU = (durationLU/2) / 29.1;
+  if(distanceLU < 100){
+    return false;
+  }
+  else{
+    Serial.print(distanceLU);
+    Serial.println(" cm");
+    return true;
+  }
+}
 
+// 우상 초음파 측정
+boolean echoRU(){
+  digitalWrite(trigPinRU, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinRU, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinRU, LOW);
+  durationRU = pulseIn(echoPinRU, HIGH);
+  distanceRU = (durationRU/2) / 29.1;
+  if(distanceRU < 100){
+    return false;
+  }
+  else{
+    Serial.print(distanceRU);
+    Serial.println(" cm");
+    return true;
+  }
+}
+*/
+// 동작
 void active(){
   if (str == "F\n"){
     go();
@@ -39,92 +83,80 @@ void active(){
   }
 }
 
+// 전진
 void go(){
-  Serial.println("직진");
-  digitalWrite(dir1, LOW);
-  digitalWrite(dir2, HIGH);
-  analogWrite(pwm1, 255);
-  analogWrite(pwm2, 255);
-  delay(500);
+  //bool LU = echoLU();
+  //bool RU = echoRU();
+  //if ((LU == true) && (RU == true)){
+    Serial.println("직진");
+    digitalWrite(dir1, LOW);
+    digitalWrite(dir2, HIGH);
+    analogWrite(pwm1, 255);
+    analogWrite(pwm2, 255);
+    delay(200); 
+  //}
 }
 
+// 좌회전
 void left(){
-  Serial.println("좌회전");
-  digitalWrite(dir1, LOW);
-  digitalWrite(dir2, HIGH);
-  analogWrite(pwm1, 0);
-  analogWrite(pwm2, 255);
-  delay(500);
+  //bool LU = echoLU();
+  //bool RU = echoRU();
+  //if ((LU == true) && (RU == true)){
+    Serial.println("좌회전");
+    digitalWrite(dir1, LOW);
+    digitalWrite(dir2, HIGH);
+    analogWrite(pwm1, 0);
+    analogWrite(pwm2, 255);
+    delay(200);
+  //}
 }
 
+// 우회전
 void right(){
-  Serial.println("우회전");
-  digitalWrite(dir1, LOW);
-  digitalWrite(dir2, HIGH);
-  analogWrite(pwm1, 255);
-  analogWrite(pwm2, 0);
-  delay(500);
+  //bool LU = echoLU();
+  //bool RU = echoRU();
+  //if ((LU == true) && (RU == true)){
+    Serial.println("우회전");
+    digitalWrite(dir1, LOW);
+    digitalWrite(dir2, HIGH);
+    analogWrite(pwm1, 255);
+    analogWrite(pwm2, 0);
+    delay(200);
+  //}
 }
 
+// 정지
 void stopm(){
-  Serial.println("정지");
-  digitalWrite(dir1, LOW);
-  digitalWrite(dir2, HIGH);
-  analogWrite(pwm1, 0);
-  analogWrite(pwm2, 0);
-  delay(500);
+  //bool LU = echoLU();
+  //bool RU = echoRU();
+  //if ((LU == false) && (RU == false)){
+    Serial.println("정지");
+    digitalWrite(dir1, LOW);
+    digitalWrite(dir2, HIGH);
+    analogWrite(pwm1, 0);
+    analogWrite(pwm2, 0);
+    delay(200);
+  //}
 }
 
+// 마스터로 텍스트 송신
 void sendToMaster() {
   Wire.write("SLAVE -> MASTER");
 }
 
 void setup() {
+  pinMode(pwm1, OUTPUT);
+  pinMode(pwm2, OUTPUT);
+  //pinMode(trigPinRU, OUTPUT);
+  //pinMode(echoPinRU, INPUT);
+  //pinMode(trigPinLU, OUTPUT);
+  //pinMode(echoPinLU, INPUT);
   Wire.begin(SLAVE);
   Wire.onReceive(receiveFromMaster);
   Wire.onRequest(sendToMaster);
   Serial.begin(9600);
-  pinMode(pwm1, OUTPUT);
-  pinMode(pwm2, OUTPUT);
-  pinMode(trigPinR, OUTPUT);
-  pinMode(echoPinR, INPUT);
-  pinMode(trigPinL, OUTPUT);
-  pinMode(echoPinL, INPUT);
 }
 
 void loop () {
-  digitalWrite(trigPinR, LOW);
-  digitalWrite(echoPinR, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPinR, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPinR, LOW);
-  durationR = pulseIn(echoPinR, HIGH);
-  
-  digitalWrite(trigPinL, LOW);
-  digitalWrite(echoPinL, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPinL, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPinL, LOW);
-  durationL = pulseIn(echoPinL, HIGH);
-
-  distanceR = durationR / 29.0 / 2.0;
-  distanceL = durationL / 29.0 / 2.0;  
-
-  if(distanceR <= 100 && distanceL <= 100){
-    stopm();
-    Serial.print("1번 초음파");
-    Serial.print(distanceR);
-    Serial.println(" cm");
-    Serial.print("2번 초음파");
-    Serial.print(distanceL);
-    Serial.println(" cm");
-  }
-  else{
-    Serial.print("초음파");
-    go();
-  }
-  delay(200);
 }
 
